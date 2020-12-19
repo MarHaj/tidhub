c52558aa4b4bef154fc33e7fe2e803cce99a15e6
marhaj <>
2020-12-19 21:31:01 +0000

    ADD: service functions framework: list_wikis(), run_wikis, stop_wikis
    FossilOrigin-Name: a9d7ac9cdec4707ac4c6fce2ca583428044e314a53847dcd83d5dbca8111a8ba

--- tidhub.sh
+++ tidhub.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+########################################
+### Functions definition
+########################################
+
+
+########################################
+# Print tidhub usage
+########################################
 usage (){
 
 cat << _EOF_
@@ -9,7 +17,7 @@ Usage:
   tidhub.sh [option [argument]]
 
 Options and their argument:
-  [-h|--help]           print this document and exit (default)
+  [-h|--help]           print this document (default)
   [-l|--list]           list all available wikis with a R flag if running
   [-r|--run]  a|keylist run all wikis or just this keylist (default first from list)
   [-s|--stop] a|keylist stop all running wikis (default) or just this keylist
@@ -23,21 +31,89 @@ Usage examples:
 
 Config file tidhubrc:
   Tidhub makes use of ~./config/tidhub/tidhubrc file that must exist and be readable.
-  It declares bash associative array of key - values pair,
-	where key is an unique identifier (name) of wiki and value is the path.
-  Example of tidhubrc file:
+  It declares bash associative array of key=values pair,
+	where key is an unique identifier (name) of the wiki and value is path to it.
+
+Example of tidhubrc file:
 
 # This file is to be sourced from tidhub.sh
 declare -A tiddirs
-# Index of tiddirs is the unique identifier (key) of the concrete Tiddlywiki instance
+# Index of tiddirs is the unique user defined identifier (key) of the wiki instance
+# Value of tiddirs is the path to the wiki instance
+#
 # User definitions:
 tiddirs[hnts]="~/Notes/home_notes/"
 tiddirs[wnts]="~/Notes/work_notes/"
 tiddirs[train]="~/Training/my_journal/"
 tidddirs[3]="~/Todo/"
+
+Requirements:
+  External programs required:
+	  Tiddlywiki on Node.js, awk, pgrep, pkill
 _EOF_
 		exit
 }
+########################################
+
+########################################
+# List wikis and mark Running ones
+#
+# Globals:
+#   tiddirs used
+#
+# Arguments
+#   none
+#
+# Outputs:
+#   list of wikis: key pid port R-flag
+#
+# Returns:
+#   none
+########################################
+list_wikis () {
+	echo "listing"
+}
+########################################
+
+########################################
+# Run all/selected wikis
+#
+# Globals:
+#   tiddirs used
+#
+# Arguments
+#   [a|keylist] wikis to run
+#
+# Outputs:
+#   input
+#
+# Returns:
+#   none
+########################################
+run_wikis () {
+	echo "run: $@"
+}
+########################################
+
+########################################
+# Stop all/selected wikis
+#
+# Globals:
+#   tiddirs used
+#
+# Arguments
+#   [a|keylist] wikis to stop
+#
+# Outputs:
+#   none
+#
+# Returns:
+#   none
+########################################
+stop_wikis () {
+	echo "stop: $@"
+}
+########################################
 
 ########################################
 # Get program options and args from positional parameters
@@ -49,20 +125,21 @@ get_opt_arg () {
 			 usage
 			;;
 		-l | --list)
-			echo "list"
+			list_wikis
 			;;
 		-r | --run)
-			echo "run"
-			# now read $2 arguments
+			shift
+			run_wikis $@
 			;;
-		-s | --stop)
-			echo "stop"
-			# now read $2 arguments
+		-s | --stop) #
+			shift
+			stop_wikis $@
 			;;
 		*)
 			[[ -z $1 ]] || echo -e "Unregognized input '$1'\n" >&2 && usage
 			;;
 	esac
+	exit
 }
 ########################################
 
